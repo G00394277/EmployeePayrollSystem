e5fd8c8b545ae83da3ee305be8c291147835f90a
G00394277 <G00394277@atu.ie>
2022-12-07 12:40:06 +0000

    Edited EmployeeTest class

--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 public class EmployeeTest
 {
-    string name = "Joe Bloggs";
-    string ppsID = "12345S";
-    int phone = "08312345678";
-    string gender = "Male";
-    string employment = "FullTime";
+    String name = "Joe Bloggs";
+    String ppsID = "12345S";
+    String phone = "08312345678";
+    String gender = "Male";
+    String employment = "FullTime";
     int age = 24;
 }
