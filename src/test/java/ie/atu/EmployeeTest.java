4aebefd65d25b64c8da3edbabaccbbbc5ebf7c1d
G00394277 <G00394277@atu.ie>
2022-12-07 12:33:55 +0000

    Added POM file Created Employee test class

---
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest
+{
+    string name = "Joe Bloggs";
+    string ppsID = "12345S";
+    int phone = "08312345678";
+    string gender = "Male";
+    string employment = "FullTime";
+    int age = 25;
+}
