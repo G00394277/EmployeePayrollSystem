37612323ac5f2f95e22e3eaee1d5d736c0d3f9b4
G00394277 <G00394277@atu.ie>
2022-12-07 12:46:52 +0000

    Started working on Employee class

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,6 +1,19 @@
 package ie.atu;
 
+
+
 public class Employee
 {
+    public static void checkName(get.name)
+    {
+        if (name.length() >= 5 && name.length <= 22)
+        {
+
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
 
 }
